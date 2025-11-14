b83be314559043dd171d4e2da29d454fdd8cd1de
joe dicks <dicks.joe@proton.me>
2025-11-13 19:59:20 -0600

    updated auth routes

---
+++ server/routes/todo.routes.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { getTodosByUserId } from "../db/queries";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { HonoEnv } from "../types";
+
+export const todos = new Hono<HonoEnv>();
+
+todos.use(authMiddleware);
+
+todos.get("/", async (c) => {
+  const user = c.get("user");
+  try {
+    const todos = await getTodosByUserId(user.id);
+    return c.json(todos);
+  } catch (error) {
+    console.error("failed to fetch todos: ", error);
+    return c.json({ error: "failed to fetch todos" }, 500);
+  }
+});
